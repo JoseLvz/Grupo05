5dc13c2b6bf7534cdf333b3dd54a27e9d73a5e16
JoseLvz <oljoselo@hotmail.com>
2018-10-27 07:45:14 -0500

    subi nuestro juego

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject DefeatMenuUI;
+    Nave nave;
+
+    void Start()
+    {
+        nave = GameObject.FindWithTag("Player").GetComponent<Nave>();
+    }
+
+    void Update()
+    {
+        //Lo que hace aparecer el menu
+        if (nave.isDead == true){
+            DefeatMenuUI.SetActive(true);
+        }
+
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void BackToMenu(){
+        SceneManager.LoadScene("Menu");
+    }
+
+    /*public void PlayGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }*/
+
+}
+
